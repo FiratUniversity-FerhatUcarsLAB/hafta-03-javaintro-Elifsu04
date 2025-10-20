d54b520622c9d993cebc06415cb628df234ab319
250541004 <250541004@firat.edu.tr>
2025-10-20 21:30:31 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,12 @@
+class UcMesaj {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hos Geldiniz");
+        System.out.println("Bilgisayar Bilimlerine Hos Geldiniz");
+        System.out.println("Proglama Eglencelidir");
+    }
+}
+Java'ya Hos Geldiniz
+Bilgisayar Bilimlerine Hos Geldiniz
+Proglama Eglencelidir
+
+=== Code Execution Successful ===
