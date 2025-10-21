9c1163828622e07118b17f128cf39f02a6e94700
250541004 <250541004@firat.edu.tr>
2025-10-21 23:33:37 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,16 @@
+class DaireHesap {
+    public static void main(String[] args) {
+        double r = 5.5;
+        double pi = 3.14159;
+        double alan = pi * r * r;
+        double cevre = 2 * pi * r;
+        System.out.println("Yaricap:" + r );
+        System.out.println("Alan:" + alan );
+        System.out.println("Cevre:" + cevre );
+    }
+}
+Yaricap:5.5
+Alan:95.0330975
+Cevre:34.55749
+
+=== Code Execution Successful ===
