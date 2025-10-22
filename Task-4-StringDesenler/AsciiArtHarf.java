d97f1cdfb369c2a774470c260a98e74341dc5761
250541004 <250541004@firat.edu.tr>
2025-10-22 22:04:49 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,30 @@
+class ASCIIArtE {
+    public static void main(String[] args) {
+        System.out.println("E Harfi ASCII Sanatı ile Çiziliyor");
+        System.out.println("====================");
+        System.out.println("********") ;
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("******");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("*");
+        System.out.println("********");
+        System.out.println("====================");
+    }
+}
+E Harfi ASCII Sanat? ile ?iziliyor
+====================
+********
+*
+*
+*
+******
+*
+*
+*
+********
+====================
+
+=== Code Execution Successful ===
