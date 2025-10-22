95e240483a6336fc734091bf352bcae96e52ba17
250541004 <250541004@firat.edu.tr>
2025-10-22 21:50:36 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,27 @@
+class IsımKartı {
+    public static void main(String[] args) {
+        String ad  = "Elif su";
+        String soyad = "Kansu";
+        String ogrNo = "250541004";
+        String bolum = "Yazilim Muh.";
+        
+        System.out.println("====================");
+        System.out.println(" OGRENCI BILGILERI ");
+        System.out.println("====================");
+        System.out.println("Ad    : " + ad);
+        System.out.println("Soyad :" + soyad);
+        System.out.println("No    :" + ogrNo);
+        System.out.println("Bolum :" + bolum);
+        System.out.println("====================");
+    }
+}
+====================
+ OGRENCI BILGILERI 
+====================
+Ad    : Elif su
+Soyad :Kansu
+No    :250541004
+Bolum :Yazilim Muh.
+====================
+
+=== Code Execution Successful ===
